0d86bccbb8af602a522a4196b3b500a047876030
Rena Kobeissi <rkobeiss@c2r6s5.42beirut.com>
2026-01-30 20:23:56 +0200

    parser 80% done

--- rena/parser/parse.c
+++ rena/parser/parse.c
@@ -6,12 +6,13 @@
 /*   By: rkobeiss <rkobeiss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 16:32:17 by rkobeiss          #+#    #+#             */
-/*   Updated: 2026/01/28 18:24:29 by rkobeiss         ###   ########.fr       */
+/*   Updated: 2026/01/30 20:18:57 by rkobeiss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 //fpr parse_pipe
+////left asssociative tree
 	//while token 
 	//take cmd = tokens while token != | 
 	//if token = | take cmd2 = right side while next is not |
@@ -23,57 +24,51 @@
 	//new left node would be the inner first node
 	//then check for type of command, keep going forward
 	//until you reach a redirection eof pipe or ) and add them to argv
-static int	count_words(t_token *t)
-{
-	int	n;
 
-	n = 0;
-	while (t && t->type != tok_pipe && t->type != tok_rparan && t->type != tok_eof)
-	{
-		if (t->type == tok_word)
-			n++;
-		t = t->next;
-	}
-	return (n);
-}
-
-t_ast	*parse_redi(t_token **curr)
+int	parse_redi(t_ast *node, t_token **curr)
 {
+	t_redir	*redi;
+	int		temp;
+
 	if (!*curr || !curr)
-		return (NULL);
-	t_ast	*node;
-	char	**redi;
-
-	if ((*curr)->type == tok_heredoc || (*curr)->type == tok_inredi || (*curr)->type == tok_outredi || (*curr)->type == tok_append)
+		return (0);
+	while ((*curr)->type == tok_heredoc || (*curr)->type == tok_inredi
+		|| (*curr)->type == tok_outredi || (*curr)->type == tok_append)
 	{
+		temp = (*curr)->type;
 		*curr = (*curr)->next;
 		if ((*curr)->type != tok_word)
-			return (NULL);
-			
+			return (0);
+		redi = malloc(sizeof(t_redir));
+		if (!redi)
+			return (0);
+		redi->type = temp;
+		redi->target = ft_strdup((*curr)->value);
+		if (!node)
+			return (0);
+		node->redir = redi;
+		redi = redi->next;
+		*curr = (*curr)->next;
 	}
+	redi->next = NULL;
+	return (1);
 }
 
 t_ast	*parse_cmd(t_token **curr)
 {
-	if (!*curr || !curr)
-		return (NULL);
 	t_ast	*node;
 	char	**cmd;
 	int		i;
-	int 	n;
+	int		n;
 
+	if (!*curr || !curr)
+		return (NULL);
 	i = 0;
 	n = count_words(*curr);
 	if (n == 0)
 		return (NULL);
 	cmd = malloc((n + 1) * sizeof(char *));
-	while (*curr && (*curr)->type != tok_pipe && (*curr)->type != tok_eof && (*curr)->type != tok_rparan)
-	{	
-		if ((*curr)->type == tok_word)
-			cmd[i++] = ft_strdup((*curr)->value);
-		*curr = (*curr)->next;
-	}
-	cmd[i] = NULL;
+	cmd_dup(*curr, **cmd);
 	node = malloc(sizeof(t_ast));
 	if (!node)
 		return (NULL);
@@ -85,35 +80,35 @@ t_ast	*parse_cmd(t_token **curr)
 	return (node);
 }
 
-t_ast	*parse_subshell(t_token **curr)
+t_ast	*parse_unit(t_token **curr)
 {
-	if (!*curr || !curr)
-		return (NULL);
 	t_ast	*node;
 	t_ast	*subshell;
 
-		if (((*curr)->type == tok_lparan))
-		{
-			*curr = (*curr)->next;
-			subshell = parse_pipe(curr);
-			if (!subshell)
-				return (NULL);
-			if (!*curr || (*curr)->type != tok_rparan)
-				return (printf("syntax error: missing )\n)", NULL));
-				*curr = (*curr)->next;
-			node = malloc(sizeof(t_ast));
-			if (!node)
-				return (NULL);
-			node->left = subshell;
-			node->right = NULL;
-			node->type = ast_subshell;
-			node->argv = NULL;
-			node->redir = NULL;
-			return (node);
-		}
-		return (parse_cmd(curr));
+	if (!*curr || !curr)
+		return (NULL);
+	if (((*curr)->type == tok_lparan))
+	{
+		*curr = (*curr)->next;
+		subshell = parse_pipe(curr);
+		if (!subshell)
+			return (NULL);
+		if (!*curr || (*curr)->type != tok_rparan)
+			return (printf("syntax error: missing )\n)", NULL));
+		*curr = (*curr)->next;
+		node = malloc(sizeof(t_ast));
+		if (!node)
+			return (NULL);
+		node->left = subshell;
+		node->right = NULL;
+		node->type = ast_subshell;
+		node->argv = NULL;
+		node->redir = NULL;
+		return (node);
+	}
+	return (parse_cmd(curr));
 }
-//left asssociative tree
+
 t_ast	*parse_pipe(t_token **curr)
 {
 	t_ast	*node;
@@ -142,4 +137,21 @@ t_ast	*parse_pipe(t_token **curr)
 	return (left);
 }
 
- 
+void	parse_main(t_token **curr)
+{
+	t_ast	*node;
+
+	if (!(*curr) || !curr)
+		return (NULL);
+	while ((*curr) && (*curr)->next)
+	{
+		if ((*curr)->type == tok_pipe)
+		{
+			node = malloc(sizeof(t_ast));
+			node = parse_pipe(curr);
+		}
+		(*curr) = (*curr)->next;
+	}
+	if ((*curr)->type != tok_eof && (*curr)->type != tok_rparan)
+		return (NULL);
+}
